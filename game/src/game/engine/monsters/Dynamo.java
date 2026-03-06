eb804065b1f7694d557a3e6ceadc0b62f5756e97
Mohamed Sherif <msherif4852@gmail.com>
2026-03-06 15:11:48 +0200

    mohamed added Dynamo

---
+++ game/src/game/engine/monsters/Dynamo.java
@@ -0,0 +1,10 @@
+package game.engine.monsters;
+import game.engine.*;
+public class Dynamo extends Monster {
+
+	public Dynamo(String name, String description, Role originalRole, int energy) {
+		super(name, description, originalRole, energy);
+		
+	}
+     
+}
