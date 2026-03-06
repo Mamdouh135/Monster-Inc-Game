ff61df2ff16c75e42bb085617ed0483d7160eeb1
mamdouhaiesh <mamdouhaiesh76@gmail.com>
2026-03-06 17:46:45 +0200

    fixed some errors and added public test

--- game/src/game/engine/Constants.java
+++ game/src/game/engine/Constants.java
@@ -2,27 +2,27 @@ package game.engine;
 
 public final class Constants {
 	// board constants
-	static int BOARD_SIZE = 100;
-	static int BOARD_ROWS = 10;
-	static int BOARD_COLS = 10;
-	static int WINNING_POSITION = 99;
-	static int STARTING_POSITION = 0;
+	public static int BOARD_SIZE = 100;
+	public static int BOARD_ROWS = 10;
+	public static int BOARD_COLS = 10;
+	public static int WINNING_POSITION = 99;
+	public static int STARTING_POSITION = 0;
 	
 	// special cells positions constants
-	static int[] MONSTER_CELL_INDICES = { 2, 18, 34, 54, 82, 88 };
-	static int[] CONVEYOR_CELL_INDICES = { 6, 22, 44, 52, 66 };
-	static int[] SOCK_CELL_INDICES = { 32, 42, 74, 84, 98 };
-	static int[] CARD_CELL_INDICES = { 4, 12, 28, 36, 48, 56, 60, 76, 86, 90 };
+	public static int[] MONSTER_CELL_INDICES = { 2, 18, 34, 54, 82, 88 };
+	public static int[] CONVEYOR_CELL_INDICES = { 6, 22, 44, 52, 66 };
+	public static int[] SOCK_CELL_INDICES = { 32, 42, 74, 84, 98 };
+	public static int[] CARD_CELL_INDICES = { 4, 12, 28, 36, 48, 56, 60, 76, 86, 90 };
 	
 	// Energy constants 
-	static int WINNING_ENERGY = 1000;
-	static int MIN_ENERGY = 0;
+	public static int WINNING_ENERGY = 1000;
+	public static int MIN_ENERGY = 0;
 	
 	// cell constants
-	static int SLIP_PENALTY = 100;
+	public static int SLIP_PENALTY = 100;
 	
 	// power constants
-	static int POWERUP_COST = 500;
+	public static int POWERUP_COST = 500;
 	
 	
 	
