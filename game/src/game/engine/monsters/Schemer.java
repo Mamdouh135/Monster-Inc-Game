904d85aadbb95d0d7f9e2f5b4fbb300c397465ff
Mohamed Sherif <msherif4852@gmail.com>
2026-03-06 15:16:29 +0200

    mohamed added Schemer

---
+++ game/src/game/engine/monsters/Schemer.java
@@ -0,0 +1,11 @@
+package game.engine.monsters;
+import game.engine.*;
+public class Schemer extends Monster {
+
+	public Schemer(String name, String description, Role originalRole,
+			int energy) {
+		super(name, description, originalRole, energy);
+		
+	}
+   
+}
