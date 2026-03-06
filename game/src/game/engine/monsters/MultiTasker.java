3aa846009793e61a4408f476dfe04665f85fd4a5
Mohamed Sherif <msherif4852@gmail.com>
2026-03-06 15:14:53 +0200

    mohamed added MultiTasker

---
+++ game/src/game/engine/monsters/MultiTasker.java
@@ -0,0 +1,21 @@
+package game.engine.monsters;
+import game.engine.*;
+public class MultiTasker extends Monster {
+     private int normalSpeedTurns;
+
+	public MultiTasker(String name, String description, Role originalRole,
+			int energy) {
+		super(name, description, originalRole, energy);
+		
+	}
+
+	public int getNormalSpeedTurns() {
+		return normalSpeedTurns;
+	}
+
+	public void setNormalSpeedTurns(int normalSpeedTurns) {
+		this.normalSpeedTurns = normalSpeedTurns;
+	}
+	
+     
+}
